6dfda7d427700ce2457e6faecc389ef442443d29
romslf <rom.laforgue@gmail.com>
2020-02-10 22:20:01 +0100

    Removed useless file + minors improvements

--- run.sh
+++ run.sh
@@ -2,19 +2,27 @@
 
 path=../ft_printf
 libname=printf.a
+timer=0.15
 
-echo "Setup starting..."
+echo "\e[96mSetup starting..."
+sleep $timer
 echo "Compiling your project..."
+sleep $timer
 make -C $path
 echo "Compiling, done."
+sleep $timer
 echo "Copying library to tester."
+sleep $timer
 cp $path/$libname printf.a
 echo "Compiling tester..."
+sleep $timer
 gcc tester.c printf.a -o tester
 echo "Compiling, done."
+sleep $timer
 rm printf.a
 echo "Setup done."
+sleep $timer
 echo "Running tester..."
+sleep $timer
 ./tester
 rm tester
-echo "Tester done."
